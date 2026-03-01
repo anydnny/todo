15bca0700b132771fd61e357ac7dae2edc394451
Daniil Glushakov <anydnny@gmail.com>
2026-03-01 19:48:47 +0300

    Удален лишний system проект

--- backend/src/modules/projects/constants/system-projects.registry.ts
+++ backend/src/modules/projects/constants/system-projects.registry.ts
@@ -8,8 +8,4 @@ export const SYSTEM_PROJECTS_REGISTRY: readonly SystemProjectRegistryItem[] = [
     id: 'd406e045-29e0-4ae3-a8b9-aed2622cb328',
     name: 'inbox',
   },
-  {
-    id: '3fc37f2a-4686-4f20-b7eb-820402978071',
-    name: 'учёба',
-  },
 ] as const;
